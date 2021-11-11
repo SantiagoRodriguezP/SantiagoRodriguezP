040a8ddbfe2032d4e214b55c3179821c77caee88
SantiagoRodriguezP <santiago.rodriguez@sinco.com.co>
2021-11-11 17:27:18 -0500

    Avances validación workout 6

--- Workouts/6 intro-component-with-signup-form/main.js
+++ Workouts/6 intro-component-with-signup-form/main.js
@@ -1,1 +1,13 @@
-form = document.querySelector("form");
+const form = document.querySelector("form");
+let input = document.querySelectorAll("input");
+let firstName = document.getElementsByClassName("name");
+let lastName = document.getElementsByClassName("last-name");
+let mail = document.getElementsByClassName("mail");
+let pass = document.getElementsByClassName("pass");
+// const email = document.getElementsByClassName("form input[type=text]");
+form.addEventListener('submit', ()=>{
+    (firstName[0].value ? firstName[0].classList.remove("error-input") : firstName[0].classList.add("error-input"));
+    (lastName[0].value ? lastName[0].classList.remove("error-input") : lastName[0].classList.add("error-input"));
+    (mail[0].value ? mail[0].classList.remove("error-input") : mail[0].classList.add("error-input"));
+    (pass[0].value ? pass[0].classList.remove("error-input") : pass[0].classList.add("error-input"));
+});
