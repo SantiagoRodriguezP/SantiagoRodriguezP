3ae88c4aa343efa5efcd920bcd411b6b3522f2a0
SantiagoRodriguezP <santiago.rodriguez@sinco.com.co>
2021-11-11 09:54:13 -0500

    Validación email Regex

--- Workouts/5 base-apparel-coming-soon/main.js
+++ Workouts/5 base-apparel-coming-soon/main.js
@@ -2,18 +2,19 @@ const form = document.querySelector("form");
 const email = document.querySelector("form input[type=email]");
 
 form.addEventListener('submit', ()=>{
-    
     let emailValue = email.value;
-    console.log(emailValue);
-    if(validateEmail(email)){
-        console.log("valido");
+    let rta = validateEmail(email);
+    if(rta==true){
         form.classList.remove('error');
     }else{
-        console.log("no valido");
         form.classList.add('error');
     }
 });
 function validateEmail(email) {
-    const re = /^(([^<>()[\]\.,;:\s@\"]+(\.[^<>()[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i;
-    return re.test(String(email).toLowerCase());
+    var validRegex = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
+    if (email.value.match(validRegex)) {
+        return true;
+    }else{
+        return false;
+    }
 }
