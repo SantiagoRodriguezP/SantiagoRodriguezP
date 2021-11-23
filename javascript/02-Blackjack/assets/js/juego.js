770d040cd02b9aec28b22e379f3f19b18046a7d4
SantiagoRodriguezP <santiago.rodriguez@sinco.com.co>
2021-11-23 14:47:52 -0500

    Sección completa Laboratorio 1 Blackjack
    Finalicé sección 5 del curso de JavaScript Moderno: Guía para dominar el lenguaje
     Udemy

--- javascript/02-Blackjack/assets/js/juego.js
+++ javascript/02-Blackjack/assets/js/juego.js
@@ -9,6 +9,18 @@ let deck = [];
 const tipos = ['C', 'D', 'H', 'S'];
 const especiales = ['A', 'J', 'Q', 'K'];
 
+let puntosJugador = 0;
+let puntosComputador = 0;
+
+// Referencias del HTML
+const btnPedir = document.querySelector('#btnPedir');
+const btnDetener = document.querySelector('#btnDetener');
+const btnNuevo = document.querySelector('#btnNuevo');
+
+const divCartasJugador = document.querySelector("#jugador-cartas");
+const divCartasComputador = document.querySelector('#computador-cartas');
+const puntosEnPantalla = document.querySelectorAll("small");
+
 //Esta función crea una nueva baraja
 const crearDeak = () => {
     for (let i = 2; i <= 10; i++) {
@@ -39,10 +51,6 @@ const pedirCarta = () => {
     }
 
     const carta = deck.pop(); // remueve el elemento y lo retorna
-
-
-    console.log(deck);
-    console.log(carta);
     return carta;
 }
 
@@ -50,9 +58,91 @@ const pedirCarta = () => {
 const valorCarta = (carta) => {
     const valor = carta.substring(0, carta.length - 1);
     return (isNaN(valor)) ?
-            (valor === 'A') ? 11 : 10
-            : valor * 1
+        (valor === 'A') ? 11 : 10
+        : valor * 1
 }
 
-const valor = valorCarta(pedirCarta());
-console.log({valor});
+// Turno de computador
+const turnoComputador = (puntosMinimos) => {
+    do {
+        const carta = pedirCarta();
+
+        puntosComputador = puntosComputador + valorCarta(carta);
+        puntosEnPantalla[1].innerText = puntosComputador;
+
+        const imgCarta = document.createElement('img');
+        imgCarta.src = `assets/cartas/${carta}.png`;
+        imgCarta.classList.add('carta');
+        divCartasComputador.append(imgCarta);
+
+        if (puntosMinimos > 21) {
+            break;
+        }
+
+    } while ((puntosComputador < puntosMinimos) && (puntosMinimos <= 21));
+
+    setTimeout(() => {
+        if (puntosComputador === puntosMinimos) {
+            alert("Nadie gana :(");
+        } else if (puntosMinimos > 21) {
+            alert("Computador gana ;)");
+        } else if (puntosComputador > 21) {
+            alert("Jugador Gana");
+        } else {
+            alert("Computador gana");
+        }
+    }, 10);
+
+}
+
+
+
+// Eventos; Callback, función que se pasa como argumento
+
+btnPedir.addEventListener('click', () => {
+    const carta = pedirCarta();
+
+    puntosJugador = puntosJugador + valorCarta(carta);
+    puntosEnPantalla[0].innerText = puntosJugador;
+
+    const imgCarta = document.createElement('img');
+    imgCarta.src = `assets/cartas/${carta}.png`;
+    imgCarta.classList.add('carta');
+    divCartasJugador.append(imgCarta);
+
+    if (puntosJugador > 21) {
+        console.warn("Perdiste");
+        btnPedir.disabled = true;
+        btnDetener.disabled = true;
+        turnoComputador(puntosJugador);
+    } else if (puntosJugador == 21) {
+        console.warn('21, ganaste');
+        btnPedir.disabled = true;
+        btnDetener.disabled = true;
+        turnoComputador(puntosJugador);
+    }
+})
+
+btnDetener.addEventListener('click', () => {
+    btnDetener.disabled = true;
+    btnPedir.disabled = true;
+    turnoComputador(puntosJugador);
+});
+
+btnNuevo.addEventListener('click', () => {
+    deck = crearDeak();
+
+    btnPedir.disabled = false;
+    btnDetener.disabled = false;
+
+    puntosComputador = 0;
+    puntosJugador = 0;
+    
+    puntosEnPantalla[0].innerText = 0;
+    puntosEnPantalla[1].innerText = 0;
+
+    divCartasComputador.innerHTML='';
+    divCartasJugador.innerHTML='';
+    console.clear();
+
+});
